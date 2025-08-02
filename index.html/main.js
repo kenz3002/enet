c024ba2651cd59a7e721acd1f0871711dbb95008
kenz3002 <toochukwumbah9@gmail.com>
2025-08-02 19:54:59 +0100

    Add files via upload

---
+++ index.html/main.js
@@ -0,0 +1,42 @@
+// alert("tdttyfdtftyd")
+
+// document.getElementById("email_button") .addEventListener("click", function(){
+//     const Email = "contact@toochukwumbah9.com";
+//     const Subject = "Book A Session";
+//     const Body = "I Want To Book For A Session";
+
+//     window.location.href = 'mailto:${email}?subject=${subject}&bodt=${body}';
+// });
+
+// const emailButton = document.getElementById("email_button");
+
+//     // Add a click event listener to the button
+//     emailButton.addEventListener("click", function () {
+//       // Email details
+//       const email = "toochukwumbah9@gmail.com";
+//       const subject = "Book a session";
+//       const body = "Hello, I want to book for a session with you";
+
+//       // Create the mailto link
+//       const mailtoLink = mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)};
+
+//       // Open the email app
+//       window.location.href = mailtoLink;
+//  const Body = "I Want To Book For A Session";
+
+//     window.location.href = 'mailto:${email}?subject=${subject}&bodt=${body}';
+// });
+
+// document.getElementById("email_button_3") .addEventListener("click", function(){
+//     const Email = "contact@toochukwumbah9.com";
+//     const Subject = "Book A Session";
+//     const Body = "I Want To Book For A Session";
+
+//     window.location.href = 'mailto:${email}?subject=${subject}&bodt=${body}';
+// });   });
+
+
+// document.getElementById("email_button_2") .addEventListener("click", function(){
+//     const Email = "contact@toochukwumbah9.com";
+//     const Subject = "Book A Session";
+    
